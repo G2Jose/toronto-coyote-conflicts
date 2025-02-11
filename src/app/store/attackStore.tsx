d8116128822b301d5045dfd0749c26d66bb622e0
George Jose <george.jose.a@gmail.com>
2025-02-11 15:49:09 -0500

    Updated details

--- src/app/store/attackStore.tsx
+++ src/app/store/attackStore.tsx
@@ -38,13 +38,10 @@ const INCIDENTS: Incident[] = [
     dogWeightLb: 15,
     numCoyotes: 1,
     location: 'June Callwood Park',
-    lat: 43.636876,
-    lng: -79.4064777,
-    notes: `We were 150 m from our condo building. I turned away for five seconds and when I looked back, the coyote lunged at my dog and was trying to rip him away. There were about four women screaming at it but it wasn't until a tall man came over to help that made the coyote leave.
-
-    My dog was attacked tonight at June park. He was leashed and the coyote literally came out of nowhere. I had to take him to emergency which was $1300 to stitch one wound and two bite marks. The coyote lunged at us and didn't care that four people were screaming and yelling. 
-
-    When I got to the emergency vet, there were two other dogs there, that also got attacked in our area - a golden doodle and a husky. The city patrols are bs and not effective and if anyone has any ideas on how we can get the city to actually act, please leave a comment.`,
+    lat: 43.636717,
+    lng: -79.40399,
+    notes:
+      "Terrier mix dog attacked by coyote at June Callwood Park, about 150m from the condo building where they live. Owner had turned away for a few seconds and when she looked back, a coyote had lunged at the dog and was trying to rip him away. There were about 4 women screaming but it wasn't until a tall man came over that the coyote left.\n\nRushed to the ER and got stitches on one wound and two bite marks. At the emergency vet, she saw two other dogs that were also attacked in the area - a golden doodle and a husky.",
   },
 ]
 
