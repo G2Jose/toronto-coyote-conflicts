9f902b5dd4a1fbbb35e4fb17d802bb99366cf58f
George Jose <george.jose.a@gmail.com>
2025-02-11 22:45:01 -0500

    Check if secret set up correctly

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  env: {
-    NEXT_PUBLIC_AIRTABLE_API_KEY: process.env.NEXT_PUBLIC_AIRTABLE_API_KEY,
-    NEXT_PUBLIC_AIRTABLE_BASE_ID: process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID,
-    NEXT_PUBLIC_AIRTABLE_TABLE_ID: process.env.NEXT_PUBLIC_AIRTABLE_TABLE_ID,
-  },
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
